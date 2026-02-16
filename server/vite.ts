859c2e24d94db6e299e806be1f468d985951adeb
Shashank Preetham <newone3549@gmail.com>
2026-02-16 16:42:19 +0530

    Lazy-load Vite config in dev server

--- server/vite.ts
+++ server/vite.ts
@@ -1,7 +1,6 @@
 import { type Express } from "express";
 import { createServer as createViteServer, createLogger } from "vite";
 import { type Server } from "http";
-import viteConfig from "../vite.config";
 import fs from "fs";
 import path from "path";
 import { nanoid } from "nanoid";
@@ -9,6 +8,7 @@ import { nanoid } from "nanoid";
 const viteLogger = createLogger();
 
 export async function setupVite(server: Server, app: Express) {
+  const { default: viteConfig } = await import("../vite.config");
   const serverOptions = {
     middlewareMode: true,
     hmr: { server, path: "/vite-hmr" },
